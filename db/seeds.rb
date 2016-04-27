bf55e911b9626f8a08a331477721f03243101450
Ben Silva <berna.loyola@gmail.com>
2016-04-27 16:44:56 -0700

    improved search to only one field

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+movies = OMDB.search('2016')
+movies.each do |movie|
+	m = OMDB.id(movie.imdb_id)
+	runtime, unit = m.runtime.split(" ")
+	runtime *= 60 if unit == "h"
+	m2 = Movie.create(
+		title: m.title, 
+		director: m.director, 
+		runtime_in_minutes: m.runtime.to_i, 
+		description: m.plot, 
+		remote_poster_url: m.poster,
+		release_date: DateTime.parse(m.released)
+	)
+end
